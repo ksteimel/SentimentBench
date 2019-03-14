3601641381c1ca5310accd654314afd1f364ee18
ksteimel <ksteimel@iu.edu>
2019-03-14 15:09:00 -0400

    Changed wget to curl since curl is already in my docker image

--- getCorpora.sh
+++ getCorpora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+curl -O http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 tar -xzvf aclImdb_v1.tar.gz
 #the unsupervised portion isn't used for this benchmark so I will be dropping it
 rm -rf aclImdb/train/unsup
