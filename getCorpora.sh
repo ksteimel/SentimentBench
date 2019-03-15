7a22f6e32aa40532e1af6e700a1d1b53fa2d4ca8
ksteimel <ksteimel@iu.edu>
2019-03-15 10:27:12 -0400

    Changed download to pull from my own servers so as to reduce my data usage

--- getCorpora.sh
+++ getCorpora.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl -O http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+# curl -O http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+curl -O https://ksteimel.duckdns.org/assets/documents/aclImdb_v1.tar.gz
 tar -xzvf aclImdb_v1.tar.gz
 #the unsupervised portion isn't used for this benchmark so I will be dropping it
 rm -rf aclImdb/train/unsup
